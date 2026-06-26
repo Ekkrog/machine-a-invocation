ac2faced51e8495c93a09afac8eabe46e50af245
Julien M <julien.morille@hotmail.fr>
2026-06-26 17:39:14 +0200

    ajout d'images pour chaque créature +  CSS(a compléter)

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const invocation = document.getElementById("invocation")
 const btnInvok = document.getElementById("btnInvok")
 const message = document.getElementById("message")
 const recommencer = document.getElementById("recommencer")
+const creature = document.getElementById("creature")
+const creatImage = document.getElementById("creatImage")
+
 // Étape 3 — Réagir au clic (rappel événements)
 
 // Direction script.js.
@@ -13,10 +16,7 @@ const recommencer = document.getElementById("recommencer")
 //     Dans la fonction, commence toujours par event.preventDefault() ! Sinon la page se recharge et ta créature s'évapore.
 //     Vérifie avec un console.log('clic !'), puis regarde la console.
 
-invocation.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("click-click");
-})
+
 // Étape 4 — Invoquer la créature (rappel DOM)
 
 // Le cœur du kata. Toujours dans la fonction de soumission :
@@ -28,15 +28,52 @@ invocation.addEventListener("click", (event) => {
 //     Cache le formulaire : document.getElementById('invocation').classList.add('cache')
 //     Affiche le résultat : document.getElementById('resultat').classList.remove('cache')
 
+const creaturesData = [
+    {
+        nom: "🐙 Chat-Octo",
+        image: "chat-octo.png",
+        description:"Te permet de chatter 8 fois plus vite"
+    },
+    {
+        nom: "🐉 Dragon-Debug",
+        image: "dragon-debug.png",
+        description:"Brûle toutes les erreurs de ta console"
+    },
+    {
+        nom: "🦄 Licorne-CSS",
+        image: "licorne-css.png",
+        description: "Donne de la couleur à ton code"
+    },
+    {
+        nom: "🦊 Renard-Refacto",
+        image: "renard-refacto.png",
+        description: "Use de sa ruse pour optimiser ton code"
+    },
+
+    {
+        nom : "🔮 God-Fémy",
+        image : "dev-fou-esoterique.png",
+        description : "Trouve les erreurs dans les énoncés. Est aussi a l'origine de Claude"
+    },
+];
+
 btnInvok.addEventListener("click", () => {
+    event.preventDefault();
+
     const prenom   = document.getElementById('prenom').value
     const creature = document.getElementById('creature').value
 
     if (prenom.length > 0 && creature !== "Choisir un familier" ) {
-        message.textContent = `${prenom}, ${creature} a entendu ton appel et te suivra tout le Bloc 1 !`
+        const creatureTrouvee = creaturesData.find(c => c.nom === creature)
+        message.textContent = `${prenom}, ${creatureTrouvee.nom}, ${creatureTrouvee.description}`
+    
+    if (creatureTrouvee) {
+        creatImage.src = creatureTrouvee.image;
+    }
         invocation.classList.add('cache')
         resultat.classList.remove('cache')
-    } else {
+        
+    }  else {
         alert("Dumbass ! Choose a name and a creature before clicking on invokate");
     }
 });
