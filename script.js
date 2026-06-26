ea504128b9f33d68df097ca99c2c137c65f9c8ba
Julien M <julien.morille@hotmail.fr>
2026-06-26 20:06:09 +0200

    changement de thème global, CSS/JS et HTML actualisés

--- script.js
+++ script.js
@@ -7,61 +7,47 @@ const recommencer = document.getElementById("recommencer")
 const creature = document.getElementById("creature")
 const creatImage = document.getElementById("creatImage")
 
-// Étape 3 — Réagir au clic (rappel événements)
 
-// Direction script.js.
-
-//     Récupère le formulaire avec getElementById).
-//     Ajoute-lui un addEventListener.
-//     Dans la fonction, commence toujours par event.preventDefault() ! Sinon la page se recharge et ta créature s'évapore.
-//     Vérifie avec un console.log('clic !'), puis regarde la console.
-
-
-// Étape 4 — Invoquer la créature (rappel DOM)
-
-// Le cœur du kata. Toujours dans la fonction de soumission :
-
-//     const prenom = document.getElementById('prenom').value
-//     const creature = document.getElementById('creature').value
-//     Construis un message, par exemple : `${prenom}, ${creature} a entendu ton appel et te suivra tout le Bloc 1 !`
-//     Affiche-le dans le <p id="message"> avec textContent.
-//     Cache le formulaire : document.getElementById('invocation').classList.add('cache')
-//     Affiche le résultat : document.getElementById('resultat').classList.remove('cache')
+// Tableau des créatures // 
 
 const creaturesData = [
     {
-        nom: "🐙 Chat-Octo",
-        image: "chat-octo.png",
-        description:"Te permet de chatter 8 fois plus vite"
-    },
-    {
         nom: "🐉 Dragon-Debug",
-        image: "dragon-debug.png",
-        description:"Brûle toutes les erreurs de ta console"
-    },
-    {
-        nom: "🦄 Licorne-CSS",
-        image: "licorne-css.png",
-        description: "Donne de la couleur à ton code"
+        image: "./images/dragon.png",
+        description: "invoque le Feu Noir pour carboniser toutes les erreurs de ta console"
     },
     {
         nom: "🦊 Renard-Refacto",
-        image: "renard-refacto.png",
-        description: "Use de sa ruse pour optimiser ton code"
+        image: "./images/renard.png",
+        description: "discerne et optimise les chemins cachés de ton code pour le rendre pur comme l'or"
+    },
+    {
+        nom: "😼 Chat-Octo",
+        image: "./images/kraken.png",
+        description: "né des profondeurs pour manipuler les océans de pixels de ton design"
+    },
+    {
+        nom: "🦄 Licorne-CSS",
+        image: "./images/licorne.png",
+        description: "illumine ton code de sa crinière luisante et son pelage ancestral"
     },
 
     {
         nom : "🔮 God-Fémy",
-        image : "dev-fou-esoterique.png",
-        description : "Trouve les erreurs dans les énoncés. Est aussi a l'origine de Claude"
+        image : "./images/dev-fou-esoterique.png",
+        description : "trouve les erreurs dans les énoncés. Est aussi a l'origine de Claude"
     },
 ];
+
+// Étape 3 — Réagir au clic (rappel événements)
 
 btnInvok.addEventListener("click", () => {
     event.preventDefault();
 
     const prenom   = document.getElementById('prenom').value
     const creature = document.getElementById('creature').value
+
+// Étape 4 — Invoquer la créature (rappel DOM)
 
     if (prenom.length > 0 && creature !== "Choisir un familier" ) {
         const creatureTrouvee = creaturesData.find(c => c.nom === creature)
@@ -70,7 +56,7 @@ btnInvok.addEventListener("click", () => {
     if (creatureTrouvee) {
         creatImage.src = creatureTrouvee.image;
     }
-        invocation.classList.add('cache')
+        formulaire.classList.add('cache')
         resultat.classList.remove('cache')
         
     }  else {
@@ -78,16 +64,9 @@ btnInvok.addEventListener("click", () => {
     }
 });
 
-
 // [Bonus] Étape 5 — Renvoyer la créature
-
-// Optionnelle. Pour refaire une invocation sans recharger la page :
-
-//     Dans la <section id="resultat">, ajoute un <button> avec l'id recommencer (texte : "Renvoyer la créature").
-//     Donne-lui un addEventListener.
-//     Dans la fonction, fais l'inverse de l'étape 4 : réaffiche le formulaire, recache le résultat.
 
 recommencer.addEventListener("click", () => {
     resultat.classList.add('cache')
-    invocation.classList.remove('cache')
+    formulaire.classList.remove('cache')
 })
